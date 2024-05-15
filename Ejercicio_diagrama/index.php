0cc9c22d12bc58e5a8906d0b7c80d63599eac40b
Ram <rmldesarrolloweb@gmail.com>
2024-05-15 15:14:40 +0200

    Diagrama

--- Ejercicio_diagrama/index.php
+++ Ejercicio_diagrama/index.php
@@ -1,6 +1,12 @@
 <?php
 require_once 'contrato.php';
+require_once 'coche.php';
 
 $contrato1 = new Contrato(Date('d-m-Y'), "15-06-2024", 17);
 echo $contrato1->showContract();
+
+
+ $coche = new Coche(5,'Audi', 'Audiguay');
+ echo $coche->getModelo();
+
  
